f092c06cd25eeca383c20b6013c02aefab93a2b4
JordanJStone <statictrack@gmail.com>
2021-01-19 15:22:47 -0700

    added files

---
+++ src/services/AxiosService.js
@@ -0,0 +1,7 @@
+// @ts-ignore
+import axios from 'axios'
+
+export const nasaApi = axios.create({
+  baseURL: 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?api_key=kagd23pJg7fGksfClu5xwbzXOnYxNthFr4fm2k5n',
+  timeout: 3000
+})
